7dd67d2842ae38de365da4ec3210dc66ada1d7c1
Matthias Herrmann <matthias.mh.herrmann@gmail.com>
2018-11-06 09:03:28 +0100

    Instances and simplification ideas for Decoder
    AIS-19112

--- src/main/scala/ch/srf/xml/Decoder.scala
+++ src/main/scala/ch/srf/xml/Decoder.scala
@@ -3,22 +3,22 @@ package ch.srf.xml
 import ch.srf.xml.util.WrapGen
 import scalaz.syntax.all._
 import scalaz.syntax.std.option._
-import scalaz.{Monad, \/}
+import scalaz.{Monad, \/, Contravariant, EitherT, Kleisli, Compose, MonadTrans, Bind}
 
 import scala.annotation.implicitNotFound
 
 @implicitNotFound("No decoder found from ${X} to ${A}")
-abstract class Decoder[F[_]:Monad, X, A] {
+//TODO isomorphic to a Kleisli[F[String \/ ?], X, A] we should consider providing a newtype and deriving
+//typeclass instances using either https://github.com/estatico/scala-newtype or by applying the iso to
+//Kleisli.
+abstract class Decoder[F[_], X, A] {
   outer =>
 
   def decode(x: X): F[String \/ A]
 
-  def ~[B](other: Decoder[F, A, B]): Decoder[F, X, B] =
-    new Decoder[F, X, B] {
-      override def decode(x: X): F[String \/ B] =
-        outer.decode(x).flatMap(_.traverseM(other.decode))
-    }
-
+  //TODO just an alias for compose. Really needed?
+  def ~[B](other: Decoder[F, A, B])
+          (implicit monadEv: Monad[F]): Decoder[F, X, B] = outer >>> other
 }
 
 trait DecoderLow2 {
@@ -63,5 +63,33 @@ object Decoder extends DecoderLow {
 
   def id[F[_]: Monad, E, I]: Decoder[F, I, I] =
     fromFunction(identity)
+
+  implicit def contravariantInstance[F[_] : Monad, C]: Contravariant[Decoder[F, ?, C]] = new Contravariant[Decoder[F, ?, C]] {
+    def contramap[A, B](r: Decoder[F, A, C])(f: B => A): Decoder[F, B, C] = new Decoder[F, B, C] {
+      override def decode(x: B): F[String \/ C] = r.decode(f(x))
+    }
+  }
+
+  implicit def monadInstance[F[_] : Monad, X]: Monad[Decoder[F, X, ?]] = new Monad[Decoder[F, X, ?]] {
+    override def point[A](a: => A): Decoder[F, X, A] = new Decoder[F, X, A] {
+      override def decode(x: X): F[String \/ A] = a.pure[String \/ ?].pure[F]
+    }
+
+    override def bind[A, B](fa: Decoder[F, X, A])(f: A => Decoder[F, X, B]): Decoder[F, X, B] = new Decoder[F, X, B] {
+      def decode(x: X): F[String \/ B] =
+        EitherT
+          .apply(fa.decode(x))
+          .flatMapF(f andThen (_.decode(x)))
+          .run
+    }
+  }
+  //TODO We can also implement MonadTrans..
+
+  implicit def composeInstance[F[_] : Monad]: Compose[Decoder[F, ?, ?]] = new Compose[Decoder[F, ?, ?]] {
+    def compose[A, B, C](f: Decoder[F, B, C], g: Decoder[F, A, B]): Decoder[F, A, C] = new Decoder[F, A, C] {
+      def decode(a: A): F[String \/ C] =
+        g.decode(a).flatMap(_.traverseM(f.decode))
+    }
+  }
 
 }
